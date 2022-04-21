b86c1a66b97380dc0ada999df71a1b5369beff74
Haddadi <haddadi@doxense.com>
2022-04-22 00:30:23 +0200

    404 page

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import "./404.scss";
+
 
 
 const Error = () => {
-    return (<h1>ErrorPAGE</h1>)
+    return (
+        <>
+            <h1 className='error_title'>404</h1>
+            <p className="error_text">Oups! La page que vous demandez n'existe pas.</p>
+            <Link to="/" className='error_link'>Retourner sur la page d’accueil</Link>
+        </>)
 }
 
 export default Error
