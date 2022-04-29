1da43be2e5652bafea9ca1c4b4d268f98a1c4a52
Maureen <haddadi.maureen@gmail.com>
2022-04-29 21:26:56 +0200

    start carousel

--- src/pages/HousingPage/Housing.tsx
+++ src/pages/HousingPage/Housing.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import Header from '../../components/Header/Header'
 import data from '../../data/data.json'
+import "./../../assets/scss/main.scss";
 
 
 
@@ -9,12 +10,25 @@ const Housing = () => {
     const id = useParams()
     console.log(id.id)
     const rental = data.find((item) => item.id === id.id);
-    console.log(rental)
+    const pictures = rental?.pictures
+    console.log(pictures)
+    const [shift , setShift] = useState(0)
     return (
         <>
             <Header />
             <div className='main'>
 
+                <div className="carousel">
+                <div className='btn'>
+                <button onClick={() => setShift(shift === 0 ? (pictures!.length * -1) + 1 : shift+1)}>Previous</button>
+                <button onClick={() => setShift(shift-1)}>Next</button>
+
+                </div>
+                {pictures?.map((img, index) => <img src={img} alt="img" style={{transform : `translateX(${shift * 100}%)`}}/>
+                )}
+               
+                </div>
+              
                 <h1>{rental?.title}</h1>
             </div></>
     )
