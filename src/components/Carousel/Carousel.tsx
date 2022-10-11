7e8c331fcfbf12af0e625c46d8056b5c058762f3
Haddadi <haddadi@doxense.com>
2022-10-11 23:58:09 +0200

    add counter to carousel

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -7,19 +7,23 @@ type Props = {
 
 const Carousel = ({ arrayPicture }: Props) => {
     const [shift, setShift] = useState(0)
+    console.log(shift)
     return (
         <div className={style.carousel}>
 
             {
-                arrayPicture.length >= 2 && <div className={style.carousel_btn_block}>   <button className={style.carousel_btn} onClick={() => setShift(shift === 0 ? (arrayPicture!.length * -1) + 1 : shift + 1)}><i className={`fa-solid fa-chevron-left ${style.carousel_i}`}></i></button>
-                    <button className={style.carousel_btn} onClick={() => setShift(shift === (arrayPicture!.length * -1) + 1 ? 0 : shift - 1)}><i className={`fa-solid fa-chevron-right ${style.carousel_i}`}></i></button></div>
+                arrayPicture.length >= 2 && <div className={style.carousel_btn_block}>   <button className={style.carousel_btn} onClick={() => setShift(shift === 0 ? arrayPicture.length - 1 : shift - 1)}><i className={`fa-solid fa-chevron-left ${style.carousel_i}`}></i></button>
+                    <button className={style.carousel_btn} onClick={() => setShift(shift === arrayPicture.length - 1 ? 0 : shift + 1)}><i className={`fa-solid fa-chevron-right ${style.carousel_i}`}></i></button></div>
+
             }
 
 
 
-            {arrayPicture.map((img, index) => <img src={img} alt="img" className={style.carousel_img} style={{ transform: `translateX(${shift * 100}%)` }} key={index} />
+            {arrayPicture.map((img, index) => <img src={img} alt="img" className={style.carousel_img} style={{ transform: `translateX(${-(shift) * 100}%)` }} key={index} />
 
             )}
+
+            <p className={style.carousel_count}>{`${shift + 1} / ${arrayPicture.length}`}</p>
 
         </div>
     )
